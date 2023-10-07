1f3987b7307cd09d807f1615bca3f4fdc15d130a
Danya <hitchgod1@gmail.com>
2023-10-07 20:20:20 +0300

    Added 4 task

---
+++ lection1/task4/main.py
@@ -0,0 +1,21 @@
+import math
+UserAnswer = str(input("Виберіть дію з числами:\n 1. +\n 2. -\n 3. *\n 4. /\n 5. корінь\n 6. степінь\n "))
+a = int(input("Введіть перше чило: "))
+b = int(input("Введіть друге чило: "))
+
+match UserAnswer:
+    case "1":
+        print("Результат = ", a+b)
+    case "2":
+        print("Результат = ", a-b)
+    case "3":
+        print("Результат = ", a*b)
+    case "4":
+        print("Результат = ", a/b)
+    case "5":
+        print("Корінь першого числа = {}, Корінь другого числа = {}".format(
+            a**0.5, b**0.5))
+    case "6":
+        FirstPowerOfNumber = int(input("Введіть степінь для першого числа: "))
+        SecondPowerOfNumber = int(input("Введіть степінь для першого числа: "))
+        print("Степінь першого числа = {}, Степінь другого числа = {}".format(a**FirstPowerOfNumber, b**SecondPowerOfNumber))
